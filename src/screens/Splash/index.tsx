a1eb6f5138b238553c5117bcd7b571f51ae2eacf
Rafael Araujo <rafa123839@gmail.com>
2023-03-17 19:04:02 -0300

    Splash Screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,27 +1,41 @@
 import React from "react";
-import Animated, {useSharedValue, useAnimatedStyle} from "react-native-reanimated";
+import
+Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withTiming,
+    Easing
+} from "react-native-reanimated";
 import { Container } from "./style";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Dimensions } from "react-native";
+
+const WIDTH = Dimensions.get("window").width
+
 export function Splash() {
     const animation = useSharedValue(0)
-    
+
     const animatedStyles = useAnimatedStyle(() => {
         return {
             transform: [
-                {translateX: animation.value}
+                { 
+                    translateX: withTiming(animation.value, {
+                        duration: 500,
+                        easing: Easing.bounce
+                    }) 
+                }
             ]
         }
     })
 
     function handleAnimationPosition() {
-        animation.value= Math.random() *190
+        animation.value = Math.random() * (WIDTH - 100)
     }
 
     return (
         <Container>
-            <Animated.View style={[styles.box, animatedStyles]}/>
+            <Animated.View style={[styles.box, animatedStyles]} />
 
-            <Button title="Mover" onPress={handleAnimationPosition}/>
+            <Button title="Mover" onPress={handleAnimationPosition} />
         </Container>
     )
 }
